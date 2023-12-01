30450361fe445e1bc58be479bb4f4522e5ebbf97
poeticDev <poeticdev23@gmail.com>
2023-12-01 14:07:20 +0900

    요청 리디렉션

--- section3_basicConcept/app4.js
+++ section3_basicConcept/app4.js
@@ -1,9 +1,12 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url; // url 값을 변수에 저장
+  const method = req.method; // 요청의 방법 저장
 
-  if (url === "/") {  // url에 따라 다른 응답을 보내도록 조건 설정
+  if (url === "/") {
+    // url에 따라 다른 응답을 보내도록 조건 설정
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(
@@ -14,8 +17,16 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
 
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    //res.writeHead(302, {}); //302 경로재지정을 의미
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
   res.setHeader("Content-type", "text/html");
-  
+
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
   res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
