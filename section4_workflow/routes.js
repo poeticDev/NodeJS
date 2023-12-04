31952b26e7d6ea722669647b203115f0720455e7
poeticDev <poeticdev23@gmail.com>
2023-12-04 15:39:40 +0900

    npm 패키지 설정하기

---
+++ section4_workflow/routes.js
@@ -0,0 +1,58 @@
+//url 확인 등 라우팅 로직
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type-"submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      //fs.writeFileSync("message.txt", message);
+      //writeFileSync는 파일 작성이 완료될 때까지 코드실행을 막는다. 즉, 다음 행을 읽을 수 없다.
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader("Content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+
+  res.end();
+};
+
+module.exports = requestHandler;
+
+// module.exports = {
+//   handler: requestHandler,
+//   someText: "Some hard coded text",
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = "Some hard coded text2";
+
+// exports.handler = requestHandler;
+// exports.someText = "Some hard coded text2";
