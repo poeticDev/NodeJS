8a7daf76c82718b47bcd117c9cbd384642cec35b
poeticDev <poeticdev23@gmail.com>
2023-12-11 17:25:19 +0900

    html 페이지 서비스하기

--- section5_express/app8.js
+++ section5_express/app8.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -9,11 +8,11 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404page.html"));
-});
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000);
--- section5_express/routes/shop.js
+++ section5_express/routes/shop.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const path = require("path"); // node 코어모듈
+
+const express = require("express");
 
 const router = express.Router();
 
-// get 방식은 정확히 이 경로인지를 체크한다.
-router.get("/", (req, res, next) => { 
-    res.send("<h1>Hello from Express!</h1>");
-  });
+router.get("/", (req, res, next) => {
+  //res.sendFile("/views/shop.html"); //운영체제의 시점에서 절대경로
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  //__dirname : 자신이 사용된 파일의 경로
+});
 
 module.exports = router;
