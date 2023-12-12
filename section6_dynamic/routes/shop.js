5cfe076318df8bd4a902901df356abf45a8fb1c4
poeticDev <poeticdev23@gmail.com>
2023-12-12 17:20:51 +0900

    요청 및 사용자 간 데이터 공유

--- section6_dynamic/routes/shop.js
+++ section6_dynamic/routes/shop.js
@@ -3,14 +3,13 @@ const path = require("path"); // node 코어모듈
 const express = require("express");
 
 const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  //res.sendFile("/views/shop.html"); //운영체제의 시점에서 절대경로
-  //res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  console.log(adminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
-  
 });
 
 module.exports = router;
