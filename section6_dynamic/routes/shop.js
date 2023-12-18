1af982d34dcc7b131b73ca74f80be1b014cfe653
poeticDev <poeticdev23@gmail.com>
2023-12-18 17:45:14 +0900

    Pug로 html 문서 만들기

--- section6_dynamic/routes/shop.js
+++ section6_dynamic/routes/shop.js
@@ -10,6 +10,7 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   const products = adminData.products;
   res.render("shop", { prods: products, docTitle: 'Shop' });
+  // 응답에 여러가지 자료를 함께 보낼 수 있다. 여기서는 객체를 보낸다.
 });
 
 module.exports = router;
