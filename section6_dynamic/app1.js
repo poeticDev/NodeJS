393232f3e0c4e015cce4d96158916748fed731f3
poeticDev <poeticdev23@gmail.com>
2023-12-19 10:12:52 +0900

    Pug로 html 문서 만들기2

--- section6_dynamic/app1.js
+++ section6_dynamic/app1.js
@@ -4,26 +4,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.set('view engine', 'pug');
 // app.set : 이 express의 전체 설정
 // view engine : 동적 템플릿을 사용함을 알림
 
-app.set("views", "views");
+app.set('views', 'views');
 //view : view 파일들이 어디에 있는지 알려줌. views폴더가 기본값이고, 만약 다른 이름으로 해당 폴더를 만들었으면 꼭 설정해야 함.
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use("/admin", adminRoutes);
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-  //res.status(404).sendFile(path.join(__dirname, "views", "404page.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "404page.html"));
 });
 
 app.listen(3000);
--- section6_dynamic/routes/admin1.js
+++ section6_dynamic/routes/admin1.js
@@ -10,8 +10,7 @@ const products = [];
 
 // /admin/add-product <- GET
 router.get("/add-product", (req, res, next) => {
-  res.render('add-product', {pageTitle: "Add Product", path: '/admin/add-product'})
-  //res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product <- POST
